9b41ecb700d0ab5f536a294e11bd3e3bb94b88cd
曹柯 <caoke.ck@alibaba-inc.com>
2015-10-22 20:58:54 +0800

    support url resource

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ module.exports = function(source) {
         var code = source.split('// module')[1].split('// exports')[0];
         var module = {};
         var exports = [];
+        var require = function(url) {
+            return url;
+        };
         eval(code);
         code = exports[0][1];
         var filename = path.basename(this.context) + '.css';
-        var filepath = path.join(this.options.output.path, filename)
+        var filepath = path.join(this.options.output.path, filename);
         fs.writeFile(filepath, code, function(err) {
             err && console.log('Generating css failed: ' + filepath);
         });
